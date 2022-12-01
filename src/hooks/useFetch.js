287aeb8595e57fd35135450f3c078cb55846f76b
Oguz Yurken <oguz.yurken@gmail.com>
2022-12-01 20:59:26 +0300

    Update Hook settings

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,15 +1,30 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
+import Config from 'react-native-config';
 
-const useFetch = url => {
+const useFetch = (url, timePeriod) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(url);
-      setData(response.data);
+      const response = await axios.get(url, {
+        params: {
+          referenceCurrencyUuid: 'yhjMzLPhuIDl',
+          timePeriod: timePeriod,
+          'tiers[0]': '1',
+          orderBy: 'marketCap',
+          orderDirection: 'desc',
+          limit: '50',
+          offset: '0',
+        },
+        headers: {
+          'X-RapidAPI-Key': Config.API_KEY,
+          'X-RapidAPI-Host': Config.API_HOST,
+        },
+      });
+      setData(response.data.data.coins);
       setLoading(false);
     } catch (err) {
       setError(err.message);
@@ -22,7 +37,7 @@ const useFetch = url => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return {data, loading, error};
+  return [data, loading, error];
 };
 
 export default useFetch;
