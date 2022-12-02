bfdc3925f384234f292593483e55e143196cc21b
Oguz Yurken <oguz.yurken@gmail.com>
2022-12-03 02:43:03 +0300

    Update HomeCard

--- src/components/Cards/HomeCard/HomeCard.style.js
+++ src/components/Cards/HomeCard/HomeCard.style.js
@@ -1,16 +1,61 @@
 import {StyleSheet} from 'react-native';
-import Colors from '../../../constants/color';
+import Colors from '../../../utils/ui/color';
+import {height, Height, width, Width} from '../../../utils/ui/dimensions';
 export default StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.primary,
   },
-  name: {
-    color: Colors.secondary,
+  innerContainer: {
+    flexDirection: 'row',
+    borderBottomWidth: 0.3,
+    borderBottomColor: 'grey',
+    padding: 15,
+    justifyContent: 'space-between',
   },
   image: {
-    width: 50,
-    height: 50,
-    resizeMode: 'contain',
+    height: height * 0.05,
+    width: width * 0.1,
+    borderRadius: 100,
+    marginRight: 15,
+    backgroundColor: Colors.primary,
+    alignSelf: 'center',
+  },
+
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    letterSpacing: 0.5,
+    marginBottom: 5,
+    color: Colors.secondary,
+  },
+  detailContainer: {
+    flexDirection: 'row',
+    marginTop: 5,
+    alignItems: 'center',
+  },
+  priceContainer: {
+    marginLeft: 'auto',
+  },
+  text: {
+    fontSize: 12,
+    fontWeight: '500',
+    letterSpacing: 0.5,
+    marginRight: 5,
+    color: Colors.secondary,
+  },
+  icon: {
+    alignSelf: 'center',
+    marginRight: 5,
+  },
+  rankContainer: {
+    backgroundColor: 'grey',
+    paddingHorizontal: 5,
+    borderRadius: 5,
+    marginRight: 5,
+  },
+  rank: {
+    fontWeight: 'bold',
+    color: Colors.secondary,
   },
 });
