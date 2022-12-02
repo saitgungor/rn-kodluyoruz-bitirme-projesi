e72b11ae788f7cfd18762934871d87541125e87a
Oguz Yurken <oguz.yurken@gmail.com>
2022-12-03 02:46:41 +0300

    Fix Folder direction for Colors

--- src/screens/Home/Home.style.js
+++ src/screens/Home/Home.style.js
@@ -1,5 +1,5 @@
 import {StyleSheet} from 'react-native';
-import Colors from '../../constants/color';
+import Colors from '../../utils/ui/color';
 export default StyleSheet.create({
   container: {
     flex: 1,
