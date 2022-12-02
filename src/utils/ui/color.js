0d8600fd0f8b9a1f358440da74bb2a4d17c1977b
Oguz Yurken <oguz.yurken@gmail.com>
2022-12-03 02:46:54 +0300

    Update Colors

---
+++ src/utils/ui/color.js
@@ -0,0 +1,11 @@
+const Colors = {
+  primary: '#121212',
+  secondary: '#FFFFFF',
+  tertiary: '#3A68FE',
+  quaternary: '#F0B90B',
+  loginButtonBackground: 'rgba(123,104,238,0.8)',
+  arrowup: '#068317',
+  arrowdown: '#da0d0d',
+};
+
+export default Colors;
