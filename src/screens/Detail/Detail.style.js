6185ffd50baa3d920de6fdc4f2c8e5592d041285
Oguz Yurken <oguz.yurken@gmail.com>
2022-12-03 02:45:35 +0300

    Create Detail Screen

---
+++ src/screens/Detail/Detail.style.js
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+import Colors from '../../utils/ui/color';
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.primary,
+  },
+});
