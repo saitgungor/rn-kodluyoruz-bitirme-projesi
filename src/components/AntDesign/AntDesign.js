cf6cc7684b0c95d0f2013c5678a3c09bfa5d42f5
Oguz Yurken <oguz.yurken@gmail.com>
2022-12-03 02:42:18 +0300

    Create AntDesign Icon

---
+++ src/components/AntDesign/AntDesign.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/AntDesign';
+const AntDesign = ({name, size, color, style}) => {
+  return <Icon name={name} size={size} color={color} style={style} />;
+};
+
+export default AntDesign;
