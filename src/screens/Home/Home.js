8c6baac63f4f99e678e863edf4dbb21d1add20bb
Oguz Yurken <oguz.yurken@gmail.com>
2022-12-03 06:30:14 +0300

    Fix import file directions

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -2,12 +2,12 @@ import {View, Text, FlatList} from 'react-native';
 import React, {useState} from 'react';
 import style from './Home.style';
 import Config from 'react-native-config';
-import useFetch from '../../hooks/useFetch';
+import useFetchCoins from '../../hooks/useFetchCoins';
 import HomeCard from '../../components/Cards/HomeCard';
 
 const Home = ({navigation}) => {
   const [timePeriod, setTimePeriod] = useState('24h');
-  const [data, loading, error] = useFetch(Config.API_URL, timePeriod);
+  const [data, loading, error] = useFetchCoins(Config.API_URL, timePeriod);
 
   console.log(data);
 
