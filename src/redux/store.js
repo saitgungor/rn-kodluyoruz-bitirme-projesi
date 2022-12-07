1444ef0c0ef9d186652aa846f8653739a03cdcfd
oğuz yürken <oguz.yurken@gmail.com>
2022-12-07 20:26:47 +0300

    Update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,18 @@
 import {configureStore} from '@reduxjs/toolkit';
 import favoriteReducer from './favoriteSlicer';
+
 import authReducer from './authSlicer';
+
 const store = configureStore({
   reducer: {
+    auth: authReducer,
     favorite: favoriteReducer,
     auth: authReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
