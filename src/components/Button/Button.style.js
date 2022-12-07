ab94c12e1fdcca2683c0eb49bc187cf8d465c15b
oğuz yürken <oguz.yurken@gmail.com>
2022-12-07 06:08:02 +0300

    Create Button Component

---
+++ src/components/Button/Button.style.js
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+import Colors from '../../utils/ui/color';
+export default StyleSheet.create({
+  pressed: {
+    opacity: 0.5,
+  },
+  button: {
+    backgroundColor: Colors.quaternary,
+    width: 300,
+    height: 50,
+    borderRadius: 10,
+    margin: 10,
+    padding: 10,
+    elevation: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: Colors.primary,
+    fontSize: 18,
+    fontWeight: '600',
+    letterSpacing: 1,
+  },
+});
