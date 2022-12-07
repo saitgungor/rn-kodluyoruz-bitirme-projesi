74b932ad6bb20ff7337506f7290dd29dc47c2e48
oğuz yürken <oguz.yurken@gmail.com>
2022-12-07 19:54:27 +0300

    Add AuthReducer in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import favoriteReducer from './favoriteSlicer';
+import authReducer from './authSlicer';
 const store = configureStore({
   reducer: {
     favorite: favoriteReducer,
+    auth: authReducer,
   },
 });
 
