37ee51990d63ca518c739a25c35ed43c7d227c07
oğuz yürken <oguz.yurken@gmail.com>
2022-12-09 05:20:32 +0300

    Delete FontAwesome component

--- src/components/FontAwesome/FontAwesome.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Icon from 'react-native-vector-icons/FontAwesome5';
-const FontAwesome = ({name, size, color, style}) => {
-  return <Icon name={name} size={size} color={color} style={style} />;
-};
-
-export default FontAwesome;
