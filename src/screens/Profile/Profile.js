dbab65beb0871e5a9dd7e89928273e57522a6327
oğuz yürken <oguz.yurken@gmail.com>
2022-12-09 05:24:08 +0300

    Update bgc

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,9 +1,9 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-
+import style from './Profile.style';
 const Profile = () => {
   return (
-    <View>
+    <View style={style.container}>
       <Text>Profile</Text>
     </View>
   );
