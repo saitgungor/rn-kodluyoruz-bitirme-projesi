9f6c0f2ab467fb38fb3c36bffdb5f6467af38f69
oğuz yürken <oguz.yurken@gmail.com>
2022-12-10 06:40:21 +0300

    Create ProfileSettings Screen

---
+++ src/screens/ProfileSettings/ProfileSettings.style.js
@@ -0,0 +1,50 @@
+import {StyleSheet} from 'react-native';
+import Colors from '../../utils/ui/color';
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.primary,
+  },
+  firstContainer: {
+    backgroundColor: Colors.primary,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignSelf: 'center',
+    marginTop: 20,
+  },
+  changeName: {
+    fontSize: 15,
+    alignSelf: 'center',
+    marginTop: 5,
+    color: Colors.bioColor,
+  },
+  secondContainer: {
+    backgroundColor: Colors.primary,
+    marginTop: 20,
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    alignSelf: 'flex-start',
+    marginTop: 10,
+    marginHorizontal: 20,
+    color: Colors.secondary,
+    letterSpacing: 1,
+  },
+  input: {
+    backgroundColor: Colors.secondary,
+    borderRadius: 10,
+    marginHorizontal: 20,
+    marginTop: 10,
+    height: 40,
+    paddingHorizontal: 10,
+  },
+  bio: {
+    height: 100,
+    textAlignVertical: 'top',
+    multiline: true,
+  },
+});
