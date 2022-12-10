5beb93b66d59af78f11d3bb3cc479a47fa2d6ff1
oğuz yürken <oguz.yurken@gmail.com>
2022-12-10 06:40:04 +0300

    Update Profile Screen

--- src/screens/Profile/Profile.style.js
+++ src/screens/Profile/Profile.style.js
@@ -3,6 +3,42 @@ import Colors from '../../utils/ui/color';
 export default StyleSheet.create({
   container: {
     flex: 1,
+    paddingBottom: 20,
+  },
+  firstContainer: {
+    flex: 1,
     backgroundColor: Colors.primary,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignSelf: 'center',
+    marginTop: 20,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    marginTop: 10,
+    color: Colors.secondary,
+  },
+  userName: {
+    fontSize: 15,
+    alignSelf: 'center',
+    marginTop: 5,
+    color: Colors.inputColor,
+  },
+  bio: {
+    fontSize: 15,
+    textAlign: 'center',
+    marginHorizontal: 50,
+    marginTop: 5,
+    color: Colors.bioColor,
+  },
+
+  secondContainer: {
+    flex: 1,
+    backgroundColor: Colors.secondary,
   },
 });
