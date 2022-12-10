67d287b04b8ac8d9ae0f0e3d559b83d6fabe5663
saitgungor <osmansaitgungor@gmail.com>
2022-12-10 23:20:35 +0300

    detail screen bug fixed

--- src/screens/Detail/Detail.js
+++ src/screens/Detail/Detail.js
@@ -4,16 +4,47 @@ import style from './Detail.style';
 import Config from 'react-native-config';
 import useFetchCoin from '../../hooks/useFetchCoin';
 import DetailCard from '../../components/Cards/DetailCard/DetailCard';
-import {useDispatch} from 'react-redux';
-import {activeProductHandler} from '../../redux/favoriteSlicer';
-import Loading from '../../components/Animations/Loading';
+import Loading from '../../components/Animations/Loading/Loading';
 import Error from '../../components/Animations/Error';
+import {useLayoutEffect} from 'react';
+import Ionicons from '../../components/Ionicons';
+import Colors from '../../utils/ui/color';
+import {useDispatch, useSelector} from 'react-redux';
+import {addFavorite, removeFavorite} from '../../redux/favoriteSlicer';
 
-const Detail = ({route}) => {
+const Detail = ({route, navigation}) => {
   const {id} = route.params;
   const dispatch = useDispatch();
-  dispatch(activeProductHandler(id));
-  const [data, loading, error] = useFetchCoin(Config.API_URL_COIN + id);
+  const [data, loading, error, timePeriod, setTimePeriod] = useFetchCoin(
+    Config.API_URL_COIN + id,
+  );
+
+  const favoriteCoins = useSelector(state => state.favorite.favorites);
+  const favorite = favoriteCoins.includes(id);
+
+  const headerButtonPressHandler = () => {
+    if (favorite) {
+      dispatch(removeFavorite(id));
+    } else {
+      dispatch(addFavorite(id));
+    }
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <Ionicons
+            name={favorite ? 'bookmark' : 'bookmark-outline'}
+            size={25}
+            color={Colors.quaternary}
+            style={{marginRight: 10}}
+            onPress={headerButtonPressHandler}
+          />
+        );
+      },
+    });
+  }, [navigation, headerButtonPressHandler]);
 
   if (loading) {
     return <Loading />;
@@ -26,7 +57,11 @@ const Detail = ({route}) => {
   return (
     <View style={style.container}>
       <ScrollView>
-        <DetailCard data={data} />
+        <DetailCard
+          data={data}
+          timePeriod={timePeriod}
+          setTimePeriod={setTimePeriod}
+        />
       </ScrollView>
     </View>
   );
