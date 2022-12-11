8492df169d9464b08d19a54639c7d9ea4cc9be53
oğuz yürken <oguz.yurken@gmail.com>
2022-12-11 18:35:07 +0300

    Change the Login and Register Screen

--- src/screens/auth/Login/Login.style.js
+++ src/screens/auth/Login/Login.style.js
@@ -1,20 +1,38 @@
 import {StyleSheet} from 'react-native';
-import {height} from '../../../utils/ui/dimensions';
+import Colors from '../../../utils/ui/color';
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
-  },
-
-  buttonContainer: {
+    alignItems: 'center',
     justifyContent: 'center',
-    height: height / 3,
+    backgroundColor: Colors.primary,
   },
-
-  formContainer: {
-    marginTop: 70,
-    zIndex: -1,
-    justifyContent: 'flex-end',
-    ...StyleSheet.absoluteFill,
+  inner_container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    backgroundColor: Colors.primary,
+    borderColor: Colors.secondary,
+    borderBottomWidth: 1,
+    width: 300,
+    height: 50,
+    borderRadius: 5,
+    margin: 10,
+    padding: 10,
+    elevation: 5,
+    color: Colors.secondary,
+  },
+  error: {
+    color: Colors.quaternary,
+  },
+  register: {
+    marginBottom: 20,
+  },
+  registerText: {
+    color: Colors.orange,
+    fontWeight: '500',
+    letterSpacing: 1,
   },
 });
--- src/screens/auth/Register/Register.style.js
+++ src/screens/auth/Register/Register.style.js
@@ -1,2 +1,38 @@
 import {StyleSheet} from 'react-native';
-export default StyleSheet.create({});
+import Colors from '../../../utils/ui/color';
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.primary,
+  },
+  inner_container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    backgroundColor: Colors.primary,
+    borderColor: Colors.secondary,
+    borderBottomWidth: 0.3,
+    width: 300,
+    height: 50,
+    borderRadius: 5,
+    margin: 10,
+    padding: 10,
+    elevation: 5,
+    color: Colors.secondary,
+  },
+  error: {
+    color: Colors.quaternary,
+  },
+  login: {
+    marginBottom: 20,
+  },
+  loginText: {
+    color: Colors.orange,
+    fontWeight: '500',
+    letterSpacing: 1,
+  },
+});
