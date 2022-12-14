7551b3a16bda35071b9b1ea363b67cb02da11b6f
oğuz yürken <oguz.yurken@gmail.com>
2022-12-14 10:38:26 +0300

    Add RegisterForm

--- src/navigation/AuthNavigation/AuthNavigation.js
+++ src/navigation/AuthNavigation/AuthNavigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from '../../screens/auth/Login';
 import Register from '../../screens/auth/Register';
+import RegisterForm from '../../screens/auth/RegisterForm';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ const AuthNavigation = () => {
       }}>
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="RegisterForm" component={RegisterForm} />
     </Stack.Navigator>
   );
 };
